9a86229c3911617e095cab834e6c0cf77202fd10
Jahzz <vcastanonh2100@alumno.ipn.mx>
2025-04-28 12:05:12 -0600

    ya sirve

--- connection.py
+++ connection.py
@@ -1,6 +1,21 @@
+from supabase import create_client
 import os
-from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+load_dotenv()
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+   
+supabase = create_client(supabase_url, supabase_key)
+
+response = supabase.table("reporte").select("*").execute()
+
+print("Respuesta completa:", response)
+    
+if response and hasattr(response, "data") and response.data:
+    data = response.data
+    print("Datos obtenidos:")
+    for row in data:
+        print(row)
+else:
+    print("No se encontraron datos o formato de respuesta inesperado.")
